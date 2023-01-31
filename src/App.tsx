827b9f9a6a8c2ab469d196f60cd408dfc130c1d4
Piroska Werner <piroska.werner@gmail.com>
2023-01-31 14:52:25 -0600

    feat: Use PokemonRow component to display pokemon in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import React from 'react'
 import './App.css'
 import pokemons from './pokemons.json'
+
+interface Pokemon {
+  id: number,
+  name: {
+    english: string,
+    japanese: string,
+    chinese: string,
+    french: string,
+  },
+  type: string[],
+  base: {
+    HP: number,
+    Attack: number,
+    Defense: number,
+    "Sp. Attack": number,
+    "Sp. Defense": number,
+    Speed: number,
+  }
+
+}
+
+const PokemonRow:React.FunctionComponent<{pokemon: Pokemon}> = ({pokemon}) => (
+  <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(', ')}</td>
+  </tr>
+)
 
 function App() {
   // console.log(pokemon.slice(0,3))
@@ -19,11 +47,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {pokemons.slice(0, 15).map(pokemon => 
-            <tr key={pokemon.id}>
-              <td>{pokemon.name.english}</td>
-              <td>{pokemon.type.join(', ')}</td>
-            </tr>
+          {pokemons.slice(0, 15).map(pokemon =>
+            <PokemonRow key={pokemon.id} pokemon={pokemon}/> 
           )}
         </tbody>
       </table>
