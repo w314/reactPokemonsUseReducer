7851d42ed8aa100db08d7b9cb9493c5230c6d32f
Piroska Werner <piroska.werner@gmail.com>
2023-02-07 10:26:55 -0600

    factor: create PokemonList componenent for pokemon table

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import './App.css'
 import pokemons from './pokemons.json'
 import { useState } from 'react'
 import Pokemon from './pokemonInterface'
-import PokemonRow from './components/PokemonRow'
+// import PokemonRow from './components/PokemonRow'
 import PokemonInfo from './components/PokemonInfo'
 import PokemonFilter from './components/PokemonFilter'
+import PokemonList from './components/PokemonList'
+
 
 function App() {
   // store the search string to filter the pokemon list
@@ -31,32 +33,17 @@ function App() {
         }}
       >
         <div>
-          <PokemonFilter filter={filter} setFilter={setFilter}/>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Power</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pokemons
-              // only take the first 15 pokemons from the list
-              .slice(0, 15)
-              // let the user search the list
-              // using toLowerCase() to make the search case insensitive (includes is case sensitive)
-              .filter(pokemon => pokemon.name.english.toLowerCase().includes(filter.toLowerCase())) 
-              // use map to display all the pokemons
-              .map(pokemon =>
-                // use PokemonRow component to display the current pokemon
-                <PokemonRow 
-                  key={pokemon.id} 
-                  pokemon={pokemon} 
-                  onSelect={(pokemon: Pokemon) => setSelectedPokemon(pokemon)}/> 
-              )}
-            </tbody>
-          </table>
+          <PokemonFilter 
+            filter={filter}
+            setFilter={setFilter}
+          />
+          <PokemonList
+            pokemons={pokemons}
+            filter={filter}
+            setSelectedPokemon={setSelectedPokemon}
+          />
         </div>
+        {/* <PokemonInfo {...selectedPokemon} /> */}
         <div>
           <h2>Selected Pokemon</h2>
           {/* use ... spread operator to pass all properties of pokemon */}
